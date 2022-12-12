deea3e92aec47ff1217f41a22f7ced3c774bfa48
dimdung <dimdung@live.com>
2022-12-12 00:36:24 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket"
+    bucket = "helixtechllc-tf-state-ado"
     key = "main"
-    region = "us-east-2"
-    dynamodb_table = "my-dynamodb-table"
+    region = "us-east-1"
+    dynamodb_table = "helixtechllc-tf-state-ado"
   }
 }
